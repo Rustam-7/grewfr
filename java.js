8da66c4fe991db73842b5fe117a4bf0cfffdc3be
Rustam-7 <joraqulovamahfuza7@gmail.com>
2025-03-19 00:23:24 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,93 @@
+let raqamlar = prompt(`raqam kiriting`)
+
+if (raqamlar > 10) {
+    alert(`sizning raqamingiz 10 dan katta `)
+}
+else{
+    alert(`sizning raqamingiz 10 dan kichkina`)
+}
+
+let transport = prompt(`transport nomini kiriting`)
+
+if (transport == `avtobus`) {
+    alert(`siz jamoat transportidan foydalanmoqdasiz`)
+    
+}
+if (transport == `mashina`) {
+    alert(`siz shaxsiy transportingizdan foydalanmoqdasiz`)
+    
+}
+if(transport == `velosiped`){
+    alert(`siz ekologik transportdan foydalanmoqdasiz`)
+}
+else{
+    alert(`Noto'gri ma'lumot `)
+}
+
+
+let sonlar = prompt(`1 dan 12 gacha raqam kiriting`)
+
+if (sonlar == `12`) {
+    alert(`Qish`)
+    
+}
+if (sonlar == `1`) {
+    alert(`Qish`)
+    
+}
+if (sonlar == `2`) {
+    alert(`Qish`)
+    
+}
+//bahor
+if (sonlar == `3`) {
+    alert(`Bahor`)
+    
+}
+if (sonlar == `4`) {
+    alert(`Bahor`)
+    
+}
+if (sonlar == `5`) {
+    alert(`Bahor`)
+    
+}
+//yoz
+
+if (sonlar == `6`) {
+    alert(`Yoz`)
+    
+}
+if (sonlar == `7`) {
+    alert(`Yoz`)
+    
+}
+if (sonlar == `8`) {
+    alert(`Yoz`)
+    
+}
+//kuz
+if (sonlar == `9`) {
+    alert(`Kuz`)
+    
+}
+if (sonlar == `10`) {
+    alert(`Kuz`)
+    
+}
+if (sonlar == `11`) {
+    alert(`Kuz`)
+    
+}
+
+
+
+let parol = prompt(`parol kiriting`)
+
+if (parol == `12345`) {
+    alert(`"Xush kelibsiz!!!"`)
+    
+}
+else(
+    alert(`"Noto'g'ri parol "`)
+)
